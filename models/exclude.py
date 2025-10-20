d6eb4d3153be58077fcc6e0964f4c51e31a5dd8b
annalaurams <nalauramoura@gmail.com>
2025-10-20 15:03:37 -0300

    update models

--- models/exclude.py
+++ models/exclude.py
@@ -2,5 +2,6 @@ EXCLUDE_KEYWORDS = [
     'kit', 'kits', 'combo', 'duo', 'dupla', "trio", 'rotina', 'corporal', 'corpo', 'hair',
     'cabelo', 'shampoo', 'condicionador', 'body', 'capilar', 'cabelo', 'hair',
     'manguito', 'meia', 'mistery', 'caixa', 'refil', 'caneta', 'geladeia', 'massageador', 'pincel', 'píncel',
-    'adesivo', 'adesivos', 'faixa', 'mini-geladeira', "maquiagem", "fragrancia", "perfume", "deodorante", "fragância",
+    'adesivo', 'adesivos', 'faixa', 'mini-geladeira', "maquiagem", "fragrancia", "perfume", "deodorante", "fragância", 
+    "coleção", "collection"
 ]
