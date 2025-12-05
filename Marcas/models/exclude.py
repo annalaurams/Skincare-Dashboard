a412af9a2b488231f9da4ab9bdaaf67b24b018bc
annalaurams <nalauramoura@gmail.com>
2025-12-05 14:51:57 -0300

    fix: ajustes finais extração

---
+++ Marcas/models/exclude.py
@@ -0,0 +1,7 @@
+EXCLUDE_KEYWORDS = [
+    'kit', 'kits', 'combo', 'duo', 'dupla', "trio", 'rotina', 'corporal', 'corpo', 'hair',
+    'cabelo', 'shampoo', 'condicionador', 'body', 'capilar', 'cabelo', 'hair',
+    'manguito', 'meia', 'mistery', 'caixa', 'refil', 'caneta', 'geladeia', 'massageador', 'pincel', 'píncel',
+    'adesivo', 'adesivos', 'faixa', 'mini-geladeira', "maquiagem", "fragrancia", "perfume", "deodorante", "fragância", 
+    "coleção", "collection"
+]
---
+++ Marcas/models/skin.py
@@ -0,0 +1,104 @@
+SKIN_TYPE_SYNONYMS_PT = {
+    "oleosa": [
+        "oleosa", "oleosidade", "óleo", "brilho excessivo",
+        "sebosa", "seborreica", "com excesso de óleo", "brilhante",
+        "cútis oleosa", "pele brilhante", "pele gordurosa", "gordurosa",
+        "cútis seborreica", "pele com brilho",
+    ],
+
+    "seca": [
+        "seca", "ressecada", "ressecamento", "desidratada",
+        "descamação", "sem óleo", "sem brilho", "aspecto áspero",
+        "pele áspera", "pele que descama", "pele craquelada",
+        "pele com coceira", "cútis seca", "pele sem hidratação"
+    ],
+
+    "mista": [
+        "mista", "pele mista", "equilíbrio irregular",
+        "pele combinada", "cútis mista"
+    ],
+
+    "normal": [
+        "normal", "equilibrada", "equilíbrio", "saudável",
+        "pele saudável", "sem problemas", "ideal", "cútis normal",
+        "pele perfeita", "sem ressecamento nem brilho", "balanceada"
+    ],
+
+    "acneica": [
+        "acneica", "propensa a acne", "com acne", "antiacne",
+        "propensa a espinhas", "pele acne", "espinhas", "cravos",
+        "comedogênica", "pele com cravos", "pele com espinhas",
+        "cútis acneica", "anti-espinhas", "controle de acne"
+    ],
+
+    "sensivel": [
+        "sensível", "facilmente irritada", "irritada", "irritação", "vermelhidão", "hipersensível",
+        "pele delicada", "reativa", "com ardência", "com coceira",
+        "cútis sensível", "intolerante", "pele que puxa", "pele fina",
+        "pele reativa", "alérgica", "com queimação", "sensivel"
+    ],
+
+    "madura": [
+        "madura", "idosa", "envelhecida", "sinais da idade",
+        "anti-idade", "antissinais", "com rugas", "flacidez",
+        "pele envelhecida", "cútis madura", "linhas de expressão",
+        "perda de firmeza", "pele com marcas do tempo"
+    ],
+
+    "com manchas": [
+        "mancha", "manchas", "hiperpigment", "melasma",
+        "desigual", "manchas escuras", "manchas solares",
+        "sardas", "hiperpigmentação", "cútis manchada", "tonalidade irregular",
+        "manchas de idade", "manchas hormonais", "pele manchada"
+    ],
+
+    "desidratada": [
+        "desidratada", "sem água", "falta de água", "ressecada",
+        "pele apertada", "sensação de repuxamento", "pele que puxa",
+        "cútis desidratada", "falta de hidratação", "água insuficiente na pele", "desidratação"
+    ],
+
+    "com poros dilatados": [
+        "poros dilatados", "poros abertos", "poros visíveis", "pele com poros",
+        "cútis com poros", "poros largos", "pele texturizada",
+        "pele com cravos abertos", "poros entupidos", "pele não refinada"
+    ],
+
+    "com rosacea": [
+        "rosacea", "rosácea", "vermelhidão facial", "pele avermelhada",
+        "vasos aparentes", "cútis com rosácea", "pele com couperose",
+        "rubor facial", "pele que cora facilmente", "pele vascular"
+    ],
+
+    "com olheiras": [
+        "olheiras", "bolsas nos olhos", "inchaço ocular", "pele cansada",
+        "olhos fundos", "manchas nos olhos", "pele sob os olhos escura",
+        "cútis periorbital cansada", "círculos escuros", "olheiras profundas", "cansaço nos olhos", "cansada"
+    ],
+
+    "com cicatrizes": [
+        "cicatrizes", "marcas de acne", "marcas", "pele marcada",
+        "cútis com cicatrizes", "crateras",
+        "sequelas de acne", "marcas pós-acne", "pele com textura irregular", "machucada"
+    ],
+    "todos os tipos": [
+        "todos os tipos", "qualquer tipo de pele", "todas as peles",
+        "independente do tipo", "todas", "universal", "para todas as peles",
+        "pele de qualquer tipo", "indiferente ao tipo de pele", "todos", "indicado para todas as peles", "indicado para todo tipo de pele"
+    ],
+}
+
+SKIN_TYPE_CANONICAL_ORDER = [
+    "acneica",
+    "com cicatrizes",
+    "com manchas",
+    "com olheiras",
+    "com poros dilatados",
+    "madura",
+    "mista",
+    "normal",
+    "oleosa",
+    "seca",
+    "sensível",
+    "todos os tipos",
+]
